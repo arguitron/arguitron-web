9c3278edaf0181e3f7d227a05e3ec3e46ed9cb06
Abel Guitron <guitronaca@gmail.com>
2022-02-25 22:19:04 -0600

    :bug: Dealing with githubpages

--- src/ArGuitron.js
+++ src/ArGuitron.js
@@ -1,13 +1,10 @@
 import React from "react";
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import { Resume } from "./components/Resume";
 
 export const ARGuitron = () => {
   return (
-    <Router>
-      <Routes>
-        <Route exact path="/" element={<Resume />} />
-      </Routes>
-    </Router>
+    <>
+      <Resume />
+    </>
   );
 };
